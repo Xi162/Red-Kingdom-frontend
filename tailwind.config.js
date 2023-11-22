e903a7bcdbc65151346036e78824b42b3de1a01c
Xi162 <huy.bui16bit@hcmut.edu.vn>
2023-11-23 00:02:03 +0700

    structure project and add default style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#D21E1E",
+        secondary: "",
+      },
+      fontFamily: {
+        sans: ['"Roboto Slab"', ...defaultTheme.fontFamily.sans],
+        title: ['"Roboto Condensed"', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
